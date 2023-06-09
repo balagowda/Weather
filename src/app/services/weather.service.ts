3f9d2ac900662cc45efcc63e892082d29db08c28
balachandregowda <balagowda9483@gmail.com>
2023-06-09 15:42:18 +0530

    simple modification

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'environments/environment';
 import { Observable } from 'rxjs/internal/Observable';
-import { environment } from 'src/environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
